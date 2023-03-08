9b5419559fcaee72ee2f0c6279477b41790484ed
furffico <jerrywang0206@gmail.com>
2023-03-08 16:41:44 +0800

    implement rust version

---
+++ furffico.rs
@@ -0,0 +1,9 @@
+
+fn main(){
+    println!("你好，现在是2023.3.8，我是{}级的{}，祝读到这段代码的同学学业顺利，事事顺心！", 2020, "Furffico");
+}
+/* 如何运行？
+ * 首先请确保您的运行环境含有rust的编译器rustc，如果没有请前往 https://www.rust-lang.org/tools/install 下载安装。
+ * 然后在命令行当前目录下输入编译指令：`rustc ./furffico.rs`，编译器会生成可执行文件 ./furffico
+ * 运行该可执行文件，即可看到输出。
+ */
